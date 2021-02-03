98ce4486c5aa65f077fb71e537b98e626b9a20aa
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-03 21:44:44 +0530

    Sum of no using fun without arg with rtn
    WAP to find sum of two no using fun without arg with rtn type

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int sum( );
+int main()
+{
+	clrscr();
+	sum();
+	getch();
+}
+int sum()
+{
+	int a=5,b=7;
+	int z=a+b;
+	cout<<"sum is:"<<z<<endl;
+     return(z);
+} 
