f987d187aa761d61d1c11c57dea5ae53e687729a
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-03 21:50:32 +0530

    Substraction using fun without arg wid rtn
    WAP to find substraction of two no using fun without arg wid return type

---
+++ substraction.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int sub( );
+int main()
+{
+	clrscr();
+	sub();
+	getch();
+}
+int sub()
+{
+	int a=30,b=20;
+	int z=a-b;
+	cout<<"substraction  is:"<<z<<endl;
+     return(z);
+} 
