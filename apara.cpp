b73f02aca33e01dd9c8e23a7da6ddcb56fa6d468
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-04 06:18:24 +0530

    Area of parallelogram using fun without arg with rtn
    WAP to find area of parallelogram using function without argument with return type

---
+++ apara.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int apara( );
+int main()
+{
+	clrscr();
+	apara();
+	getch();
+}
+int apara()
+{
+	float b=30,h=2;
+	float A=b*h;
+	cout<<"Area of parallelogram is:"<<A<<endl;
+     return(A);
+} 
