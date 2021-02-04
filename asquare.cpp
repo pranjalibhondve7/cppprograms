f866bd35f4316f5346d590ab06aa3937e4828077
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-04 06:07:21 +0530

    Area of square using fun without arg with rtn
    WAP to find area of square using function without argument with return type

---
+++ asquare.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int asqrt( );
+int main()
+{
+	clrscr();
+	asqrt();
+	getch();
+}
+int asqrt()
+{
+	float a=30;//,b=2;
+	float A=a*a;
+	cout<<"Area of square is:"<<A<<endl;
+     return(A);
+} 
