ce795e8e8c5e39615d578d67c66a01f441fe9b0d
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-04 05:56:27 +0530

    Multiplication of two no using fun without arg with rtn
    WAP to find multiplication of two no using function without argument with return type

---
+++ multiplication.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int mul( );
+int main()
+{
+	clrscr();
+	mul();
+	getch();
+}
+int mul()
+{
+	int a=30,b=20;
+	int z=a*b;
+	cout<<"Multiplication   is:"<<z<<endl;
+     return(z);
+} 
