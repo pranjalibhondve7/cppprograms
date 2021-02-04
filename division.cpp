ef74b0698e44d3e7cecf6c747089c368d766ce5d
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-04 05:59:57 +0530

    Division using fun without arg with rtn
    WAP to find division of two no using function without argument with return type

---
+++ division.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int div( );
+int main()
+{
+	clrscr();
+	div();
+	getch();
+}
+int div()
+{
+	float a=30,b=2;
+	float z=a/b;
+	cout<<"division  is:"<<z<<endl;
+     return(z);
+} 
