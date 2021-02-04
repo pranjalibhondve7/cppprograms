0bbf4da1e7bfa93f5ae72be6bbff3db9dbae65d6
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-04 06:37:34 +0530

    Perimeter of square using fun without arg with rtn
    WAP to find perimeter of square using function without argument with return type

---
+++ psquare.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+int psqrt( );
+int main()
+{
+	clrscr();
+	psqrt();
+	getch();
+}
+int psqrt()
+{
+	float a=30;//r2=5;
+	float p=4*a;
+	cout<<"Perimeter of square  is:"<<p<<endl;
+     return(p);
+} 
