2f9c7b9391dfded465d80e8e259096dd59c0860b
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-05 13:15:51 +0530

    Volume of cube using inline function

---
+++ inline.cpp
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+#include<iostream.h>
+class student{
+	public:
+int s,v;
+     inline int vcube(int s)
+     {
+     	return s*s*s;
+     }
+     
+}e;
+int main()
+{
+
+	cout<<"the cube of three is :"<<e.vcube(3)<<endl;
+	return 0;
+}
