869078755c1f69d70fcdf347d0785edf61af8b0d
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-11 12:41:39 +0530

    Pattern 11
    EDCBA
    EDCBA
    EDCBA
    EDCBA

---
+++ pattern11.cpp
@@ -0,0 +1,23 @@
+/*EDCBA
+    EDCBA
+    EDCBA
+    EDCBA*/
+
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   char i, j, num;
+  // cout<<"Enter the number of rows: ";
+  // cin>>num;
+   for (i ='A'; i <='D';i++) 
+   {
+      for (j ='E'; j >= 'A';j--) 
+      {
+         cout<<j;
+         
+      }
+      cout<<endl;
+   }
+   getch();
+}
