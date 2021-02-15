9b497ba1643b5668c42e06d71ca239965c1525ac
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-15 10:33:02 +0530

    Pattern 1

---
+++ pattern1.cpp
@@ -0,0 +1,23 @@
+/* *****
+     *****
+     *****
+     *****  */
+
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   char i, j, num;
+  // cout<<"Enter the number of rows: ";
+  // cin>>num;
+   for (i=1; i <=5;i++) 
+   {
+      for (j =1; j <= 4;j++) 
+      {
+         cout<<"*";
+         
+      }
+      cout<<endl;
+   }
+   getch();
+}
