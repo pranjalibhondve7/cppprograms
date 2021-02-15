b0fe4c55dd3e563e0a48ea148a6a2f0977030324
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-15 10:48:49 +0530

    Add files via upload
    12345
    1234
    123
    12
    1

---
+++ pattern2.cpp
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n;
+	cout<<"Enter the no. of rows : ";
+	cin>>n;
+	
+	for(i=n;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+		cout<<j;
+		
+	    }
+	cout<<endl;
+
+}
+}
