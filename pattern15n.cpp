242b5f29eb3693d6136639aa968b7a927f0d11f3
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-15 12:02:02 +0530

    Pattern 15
       *
     *. *
    * *  *

---
+++ pattern15n.cpp
@@ -0,0 +1,40 @@
+
+
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int i, j, num,k,m=1;
+   cout<<"Enter the number of rows: ";
+  cin>>num;
+   for (i=1; i <=num;i++) 
+   {
+     
+      for (j =1; j <=num;j++) 
+      {
+         cout<<" ";
+      }
+      num--;
+         for ( k=1;k<=i;k++)
+         {
+              cout<<"* " ;
+         }
+         cout<<endl;
+   }
+  /* for (i=1; i <=num;i++) 
+   {
+     cout<<" ";
+      for (j =1; j <=m;j++) 
+      {
+         cout<<" ";
+      }
+      m++;
+         for ( k=num;k>=i;k--)
+         {
+              cout<<" *" ;
+         }
+         cout<<endl;
+   }*/
+      
+   getch();
+}
