2d7091cd079b2855248cb4b284a29a8f5f48ffc7
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-15 11:16:24 +0530

    Pattern 6
    *****
    *.     *
    *.     *
    *.     *
    *****

---
+++ pattern6n.cpp
@@ -0,0 +1,24 @@
+
+
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int i, j, num;
+   cout<<"Enter the number of rows: ";
+  cin>>num;
+   for (i=1; i <=num;i++) 
+   {
+      for (j =1; j <=num;j++) 
+      {
+         if(i==1||i==num||j==1||j==num)
+         cout<<"*";
+         else
+         cout<<" ";
+         
+      }
+      cout<<endl;
+   }
+      
+   getch();
+}
