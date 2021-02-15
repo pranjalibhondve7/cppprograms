db0e0c63848c2bf64049a81587d70f83f8234f14
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-15 11:34:40 +0530

    Pattern 11
    1
    12
     123
    1234

---
+++ pattern11n.cpp
@@ -0,0 +1,21 @@
+
+
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int i, j, num,k;
+   cout<<"Enter the number of rows: ";
+  cin>>num;
+   for (i=1; i <=num;i++) 
+   {
+     
+      for (j =1; j <=i;j++) 
+      {
+         cout<<j;
+        }
+      cout<<endl;
+   }
+      
+   getch();
+}
