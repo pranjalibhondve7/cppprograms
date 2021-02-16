9a1998df10909a950388626ae551a6190a85011c
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-16 13:20:48 +0530

    Mix pattern
    54321
    *****
    54321
    *****

---
+++ mix.cpp
@@ -0,0 +1,27 @@
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int i, j, n,k;
+//  char ch ='A';
+  //cout<<"Enter the number of rows: ";
+  //cin>>n;
+  
+   for (i=1; i <=2;i++) 
+   {
+     // cout<<i;
+      for (j =5; j >= 1;j--)
+                {
+                  cout<<j;
+                }
+              cout<<endl;
+  
+              for (k=1;k<=5;k++)
+              {
+                  cout<<"*";
+              }
+  
+      cout<<endl;
+   }
+   getch();
+}
