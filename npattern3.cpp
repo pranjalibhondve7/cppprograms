d4fd7ac00d43f0ec586e25b393468929749aba80
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-16 21:08:47 +0530

    pattern 3
         1
        12
       123
     1234 
    12345

---
+++ npattern3.cpp
@@ -0,0 +1,25 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j,n,k;
+	cout<<"Enter the no. of rows : ";
+	cin>>n;
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=n-1;j>=i;j--)
+		{
+           cout<<" ";
+       }
+		for(k=1;k<=i;k++)
+		{
+	        cout<<k;
+	    }
+	    cout<<endl;
+	}
+	
+return 0;
+}
