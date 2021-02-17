4a66d59a0f11da77f15239bb65b7ed5745f3bf99
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-17 06:19:03 +0530

    Pattern 8
    AAAAA
      AAAA
         AAA
            AA
               A

---
+++ npattern8.cpp
@@ -0,0 +1,22 @@
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int i, j, n,k;
+   char ch='A';
+   cout<<"Enter the number of rows: ";
+   cin>>n;
+   for (i=n; i >=1;i--) 
+   {
+     for(k=n-1;k>=i;k--)
+      cout<<" ";
+      for (j =1; j <=i;j++) 
+      {
+         cout<<ch;
+      //   ch++;
+      }
+      //ch++;
+     cout<<endl;
+   getch();
+}
+}
