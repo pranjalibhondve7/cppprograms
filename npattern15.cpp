3a7340cecf026e4589e701f525cd900467e7dfb1
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-17 13:41:34 +0530

    Pattern 15
       1
     123
    12345

---
+++ npattern15.cpp
@@ -0,0 +1,22 @@
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int n,k,s=1;
+   int  i, j;
+   //char ch='A';
+   cout<<"Enter the number of rows: ";
+   cin>>n;
+   for (i=1; i <=n;i++) 
+   {
+     for(k=n-1;k>=i;k--)
+      cout<<" ";
+      for (j =1; j <=s;j++) 
+      {
+         cout<<j  ;
+      }
+    s=s+2;
+     cout<<endl;
+   }
+   getch();
+}
