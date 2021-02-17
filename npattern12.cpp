cbc9eae422b17589ff71576128de67c676054d74
pranjalibhondve7 <78415062+pranjalibhondve7@users.noreply.github.com>
2021-02-17 10:23:52 +0530

    Pattern 12
           A
         B B
       C C C
      D D D D

---
+++ npattern12.cpp
@@ -0,0 +1,23 @@
+#include <conio.h>
+#include<iostream.h>
+int main() 
+{
+   int n,k;
+   int i, j;
+   char ch='A';
+   cout<<"Enter the number of rows: ";
+   cin>>n;
+   for (i=1; i <=n;i++) 
+   {
+     for(k=n-1;k>=i;k--)
+      cout<<" ";
+      for (j =1; j <=i;j++) 
+      {
+         cout<< ch<<" ";
+        // ch++;
+      }
+    ch++;
+     cout<<endl;
+   getch();
+}
+}
